ae1f9b61ff5c8467acbd3509a81a7382bb5cd126
gracequaye1 <graceokaileyquaye360@gmail.com>
2024-10-29 19:24:19 -0700

    Added string_indexing.py

---
+++ Python_Basics/string_indexing.py
@@ -0,0 +1,13 @@
+sentence = "Python is powerful and easy to learn."
+
+# Length of the string
+print(len(sentence))
+
+# First character
+print(sentence[0])
+
+# Last character
+print(sentence[-1])
+
+# Extract the word "powerful"
+print(sentence[10:19])
