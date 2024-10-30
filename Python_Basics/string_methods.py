d87fe5fd3ea0f1753b32c181b61e848c9ce7bbdf
gracequaye1 <graceokaileyquaye360@gmail.com>
2024-10-29 19:13:02 -0700

    added string_methods.py

---
+++ Python_Basics/string_methods.py
@@ -0,0 +1,14 @@
+text = "Hello, World"
+
+# Convert to uppercase
+print(text.upper())
+
+# Convert to lowercase
+print(text.lower())
+
+# Replace "World" with "Python"
+print(text.replace("World", "Python"))
+
+# Join example
+words = ["Python", "is", "awesome"]
+print(" ".join(words))
